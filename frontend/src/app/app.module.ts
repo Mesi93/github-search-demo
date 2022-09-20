018a419a6884a925eb8f179a60fde0bb78468d16
Garamvölgyi Emese <garamvolgyie@molaris.local>
2022-09-20 12:29:50 +0200

    base structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPageComponent } from './feature/search/page/search-page/search-page.component';
+import { HistoryPageComponent } from './feature/history/page/history-page/history-page.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { AngularMaterialModule } from './angular-material.modules';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent,
+    HistoryPageComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
